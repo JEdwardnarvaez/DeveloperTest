2e2aa3803c79da10ecbe5b0b607dfe5b974f51dc
John Edward <edwardnarvaez@utp.edu.co>
2020-08-23 22:51:38 -0500

    intervenciones finales

--- exercise2.js
+++ exercise2.js
@@ -4,32 +4,32 @@ function ultima_posicion (pila) {
     return pila[pila.length - 1]
 }
 
-function balanceo(cadena) {
-    let pila = []
-    pila.push()
-    pila.pop()
+function balanceo(cadena) { // cadeba que se desea evaluar
+    let pila = [] // Pila vacía
+    pila.push() // Ingresar en pila
+    pila.pop() // Eliminar de la pila
 
-    for (let i=0; i<cadena.length; i++) {
-        let caracter = cadena.charAt(i)
-        if (caracter === '(') {
-            pila.push(caracter)
-        } else if (caracter === ')') {
-            if(ultima_posicion(pila) === '(') {
-                pila.pop()
+    for (let i=0; i<cadena.length; i++) { //ciclo que recorerá cada símbolo de la cadena
+        let caracter = cadena.charAt(i) 
+        if (caracter === '(') { // El caracter en x posición es '(' ?
+            pila.push(caracter) // Si es así, lo ingresa a la pila
+        } else if (caracter === ')') { // Si el caracter no es '(' entonces
+            if(ultima_posicion(pila) === '(') { // pregunta si antes estaba '('
+                pila.pop() // de ser así, elimina el caracter '(' y ')' nunca ingresa
             } else {
                 return false;
             } 
         }
     }
-    return pila.length === 0
+    return pila.length === 0 // Pregunta si la pila ahora está vacía y entrega True
 }
 
-const answer = balanceo("2*(3-a))") == true ? 'correct': 'incorrect';
+const answer = balanceo("a+(b*c)-2-a") == true ? 'correct': 'incorrect';
 console.log(answer);
 
-
+/*
 console.log(balanceo("a+(b*c)-2-a"))
-console.log(balanceo("(a+b*(2-c)-2+a)*2 "))
-console.log(balanceo("(a*b-(2+c) "))
+console.log(balanceo("(a+b*(2-c)-2+a)*2"))
+console.log(balanceo("(a*b-(2+c)"))
 console.log(balanceo("2*(3-a))"))
-console.log(balanceo(")3+b*(2-c)("))
+console.log(balanceo(")3+b*(2-c)("))*/
